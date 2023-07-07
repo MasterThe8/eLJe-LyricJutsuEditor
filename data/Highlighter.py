92e2e26dfbdd8e9815b7924cc374d4a40e48e375
MasterThe8 <68697265+MasterThe8@users.noreply.github.com>
2023-07-07 20:12:03 +0700

    delete comment in highlighter

--- data/Highlighter.py
+++ data/Highlighter.py
@@ -11,14 +11,6 @@ from PyQt5.QtCore import *
 class Highlighter(QSyntaxHighlighter):
     def __init__(self, parent=None):
         super(Highlighter, self).__init__(parent)
-        
-        # Warna untuk masing-masing jenis teks
-        # keywordFormat = QTextCharFormat()
-        # keywordFormat.setForeground(Qt.darkBlue)
-        # keywordFormat.setFontWeight(QFont.Bold)
-        # keywords = ['if', 'else', 'while', 'for', 'def', 'return']
-        # self.highlightingRules = [(QRegExp('\\b' + keyword + '\\b'), keywordFormat)
-        #                           for keyword in keywords]
 
         keywordFormat = QTextCharFormat()
         keywordFormat.setForeground(Qt.blue)
@@ -28,11 +20,6 @@ class Highlighter(QSyntaxHighlighter):
                                   (QRegExp("\\belse\\b"), keywordFormat),
                                   (QRegExp("\\bswitch\\b"), keywordFormat)]
 
-        # eventFormat = QTextCharFormat()
-        # eventFormat.setForeground(QColor('#03FCF4'))
-        # eventNames = ['Default','default','phrase_start','phrase_end','lyric ','idle','play','half_tempo','normal_tempo','verse','chorus','music_start','lighting ()','lighting (flare)','lighting (blackout)','lighting (chase)','lighting (strobe)','lighting (color1)','lighting (color2)','lighting (sweep)','crowd_lighters_fast','crowd_lighters_off','crowd_lighters_slow','crowd_half_tempo','crowd_normal_tempo','crowd_double_tempo','band_jump','sync_head_bang','sync_wag']
-        # self.highlightingRules = [(QRegExp('\b' + eventName + '\b'), eventFormat) for eventName in eventNames]
-        
         classFormat = QTextCharFormat()
         classFormat.setFontWeight(QFont.Bold)
         classFormat.setForeground(Qt.white)
@@ -50,23 +37,16 @@ class Highlighter(QSyntaxHighlighter):
         htmlFormat = QTextCharFormat()
         htmlFormat.setForeground(QColor('#00FF7F')) 
         self.highlightingRules.append((QRegExp('<[^>]+>'), htmlFormat))
-
-        defaultFormat = QTextCharFormat()
-        defaultFormat.setForeground(QColor('#03FCF4'))
-        self.highlightingRules.append((QRegExp('Default'), defaultFormat))
         
         # Event Highlight
-        # eventName = ['Default','default','phrase_start','phrase_end','lyric ','idle','play','half_tempo','normal_tempo','verse','chorus','music_start','lighting ()','lighting (flare)','lighting (blackout)','lighting (chase)','lighting (strobe)','lighting (color1)','lighting (color2)','lighting (sweep)','crowd_lighters_fast','crowd_lighters_off','crowd_lighters_slow','crowd_half_tempo','crowd_normal_tempo','crowd_double_tempo','band_jump','sync_head_bang','sync_wag']
         eventFormat = QTextCharFormat()
         eventFormat.setForeground(QColor('#03FCF4'))
-        eventName = ["Default","default", "Section", "section", "phrase_start","phrase_end","lyric","idle","play","half_tempo","normal_tempo","verse","chorus","music_start","lighting ()","lighting (flare)","lighting (blackout)","lighting (chase)","lighting (strobe)","lighting (color1)","lighting (color2)","lighting (sweep)","crowd_lighters_fast","crowd_lighters_off","crowd_lighters_slow","crowd_half_tempo","crowd_normal_tempo","crowd_double_tempo","band_jump","sync_head_bang","sync_wag"]
+        eventName = ["Default","default", "Section", "section", "phrase_start","phrase_end","lyric","idle","half_tempo","normal_tempo","verse","chorus","music_start","lighting ()","lighting (flare)","lighting (blackout)","lighting (chase)","lighting (strobe)","lighting (color1)","lighting (color2)","lighting (sweep)","crowd_lighters_fast","crowd_lighters_off","crowd_lighters_slow","crowd_half_tempo","crowd_normal_tempo","crowd_double_tempo","band_jump","sync_head_bang","sync_wag"]
 
         for i in eventName:
             temp = i
             self.highlightingRules.append((QRegExp(temp), eventFormat))
             
-
-
     def highlightBlock(self, text):
         for pattern, format in self.highlightingRules:
             expression = QRegExp(pattern)
